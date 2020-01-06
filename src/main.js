1231ca5baec53c33f5e03b917b2a895a9803fc2b
Jiao Jian <903939365@qq.com>
2020-01-06 20:23:59 +0800

    项目初始化-style样式目录初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './utils/index'
+import './utils/vants.js'
+
+import './styles/index.css'// 引入全局样式
 
 Vue.config.productionTip = false
 
